5a494ab46ea01782fa475f8bd8c24b29507a1b52
Takayuki Komatsu <komatsutakayuki235@live.com>
2022-05-01 22:17:34 -0300

    test: ensure BcryptAdapter throws if bcrypt throws

--- src/infra/cryptography/bcrypt-adapter.spec.ts
+++ src/infra/cryptography/bcrypt-adapter.spec.ts
@@ -3,13 +3,12 @@ import { BcryptAdapter } from './bcrypt-adapter'
 
 jest.mock('bcrypt', () => ({
   async hash (): Promise<string> {
-    return new Promise(resolve => resolve('hash'))
+    return 'hash'
   }
 }))
 
 const salt = 12
-
-const makeSut = ():BcryptAdapter => {
+const makeSut = (): BcryptAdapter => {
   return new BcryptAdapter(salt)
 }
 
@@ -25,5 +24,12 @@ describe('Bcrypt Adapter', () => {
     const sut = makeSut()
     const hash = await sut.encrypt('any_value')
     expect(hash).toBe('hash')
+  })
+
+  test('Should throw if bcrypt throws', async () => {
+    const sut = makeSut()
+    jest.spyOn(bcrypt, 'hash').mockImplementationOnce(() => { throw new Error() })
+    const promise = sut.encrypt('any_value')
+    await expect(promise).rejects.toThrow()
   })
 })
