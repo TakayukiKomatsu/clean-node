4f76880ec148122ba7c5e716799d82a6ca7a30ef
Takayuki Komatsu <komatsutakayuki235@live.com>
2022-05-01 21:19:37 -0300

    test: ensure DbAddAccount throws if AddAccountRepository throws

--- src/data/useCases/add-account/db-add-account.spec.ts
+++ src/data/useCases/add-account/db-add-account.spec.ts
@@ -83,4 +83,16 @@ describe('DbAddAccount', () => {
       password: 'hashed_password'
     })
   })
+
+  test('Should throw if AddAccountRepository if add throws', async () => {
+    const { sut, addAccountRepositoryStub } = makeSut()
+    jest.spyOn(addAccountRepositoryStub, 'add').mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error())))
+    const accountData = {
+      name: 'valid_name',
+      email: 'valid_email@mail.com',
+      password: 'valid_password'
+    }
+    const promise = sut.add(accountData)
+    expect(promise).rejects.toThrow()
+  })
 })
